31f7a82dbe7b92fdf7b8afaf0163fb6977a7b008
MiKraska <mikraska@gmail.com>
2020-01-06 11:38:32 +0100

    19.2 z błędem

---
+++ src/main/java/com/crud/tasks/mapper/TaskMapper.java
@@ -0,0 +1,35 @@
+package com.crud.tasks.mapper;
+
+import com.crud.tasks.domain.Task;
+import com.crud.tasks.domain.TaskDto;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class TaskMapper {
+
+    public Task mapToTask(final TaskDto taskDto) {
+        return new Task(
+                taskDto.getId(),
+                taskDto.getTitle(),
+                taskDto.getContent());
+    }
+
+    public TaskDto mapToTaskDto(final Task task) {
+        return new TaskDto(
+                task.getId(),
+                task.getTitle(),
+                task.getContent());
+    }
+
+    public List<TaskDto> mapToTaskDtoList(final List<Task> taskList) {
+        List<TaskDto> list = new ArrayList<>();
+        for (Task t : taskList) {
+            TaskDto taskDto = new TaskDto(t.getId(), t.getTitle(), t.getContent());
+            list.add(taskDto);
+        }
+        return list;
+    }
+}
